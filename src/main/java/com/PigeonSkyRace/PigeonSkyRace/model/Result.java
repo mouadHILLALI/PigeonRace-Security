742b245c58b360d32624a364b34ec403060b27b2
mouadHILLALI <mauadtoto001@gmail.com>
2024-12-02 13:01:32 +0100

    PIG-2 setting up the database and reconfiguring the project

--- src/main/java/com/PigeonSkyRace/PigeonSkyRace/model/Result.java
+++ src/main/java/com/PigeonSkyRace/PigeonSkyRace/model/Result.java
@@ -11,7 +11,6 @@ import java.time.Duration;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Document("results")
 public class Result {
     @DBRef
@@ -23,106 +22,11 @@ public class Result {
     private double points;
 
 
-    public Result(){
+    public Result() {
 
     }
+
     public Result(String pigeon, double distance, double v, double speed, double v1, double points) {
 
-
-    public String getPigeon() {
-        return pigeon;
-    }
-
-    public void setPigeon(String pigeon) {
-        this.pigeon = pigeon;
-    }
-
-    public double getDistance() {
-        return distance;
-    }
-
-    public void setDistance(double distance) {
-        this.distance = distance;
-    }
-
-    public Duration getFlightTime() {
-        return flightTime;
-    }
-
-    public void setFlightTime(Duration flightTime) {
-        this.flightTime = flightTime;
-    }
-
-    public double getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
-    public double getAdjustmentCoefficient() {
-        return adjustmentCoefficient;
-    }
-
-    public void setAdjustmentCoefficient(double adjustmentCoefficient) {
-        this.adjustmentCoefficient = adjustmentCoefficient;
-    }
-
-    public double getPoints() {
-        return points;
-    }
-
-    public void setPoints(double points) {
-        this.points = points;
-
-    }
-
-    public String getPigeon() {
-        return pigeon;
-    }
-
-    public void setPigeon(String pigeon) {
-        this.pigeon = pigeon;
-    }
-
-    public double getDistance() {
-        return distance;
-    }
-
-    public void setDistance(double distance) {
-        this.distance = distance;
-    }
-
-    public Duration getFlightTime() {
-        return flightTime;
-    }
-
-    public void setFlightTime(Duration flightTime) {
-        this.flightTime = flightTime;
-    }
-
-    public double getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
-    public double getAdjustmentCoefficient() {
-        return adjustmentCoefficient;
-    }
-
-    public void setAdjustmentCoefficient(double adjustmentCoefficient) {
-        this.adjustmentCoefficient = adjustmentCoefficient;
-    }
-
-    public double getPoints() {
-        return points;
-    }
-
-    public void setPoints(double points) {
-        this.points = points;
     }
 }
