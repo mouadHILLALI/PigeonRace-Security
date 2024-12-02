b35a9f709a4d4028310571af49e4a030fcf56cd1
mouadHILLALI <mauadtoto001@gmail.com>
2024-12-03 00:20:00 +0100

    PIG-3 created CustomUserDetailsService to fetch Username and pass and return UserDetail object

--- src/main/java/com/PigeonSkyRace/PigeonSkyRace/model/User.java
+++ src/main/java/com/PigeonSkyRace/PigeonSkyRace/model/User.java
@@ -10,14 +10,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -28,4 +34,24 @@ public class User {
     @NotNull
     private UserRoles role = UserRoles.ROLE_USER;
     private boolean isEnabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
