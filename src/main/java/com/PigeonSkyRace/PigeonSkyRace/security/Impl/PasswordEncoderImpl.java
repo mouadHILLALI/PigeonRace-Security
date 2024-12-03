d641ef21a8a85cb0ee3b5f28d60abc946d651e81
mouadHILLALI <mauadtoto001@gmail.com>
2024-12-03 01:12:05 +0100

    PIG-4-Password-Encoding

---
+++ src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/Impl/PasswordEncoderImpl.java
@@ -0,0 +1,10 @@
+package com.PigeonSkyRace.PigeonSkyRace.security.Impl;
+
+import com.PigeonSkyRace.PigeonSkyRace.security.PasswordEncoder;
+
+public class PasswordEncoderImpl implements PasswordEncoder {
+    @Override
+    public String encode(String password) {
+        return "";
+    }
+}
---
+++ src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/PasswordEncoder.java
@@ -0,0 +1,5 @@
+package com.PigeonSkyRace.PigeonSkyRace.security;
+
+public interface PasswordEncoder {
+    public String encode(String password);
+}
