8dfab0624abbd283fe52b84153cdfed9698ede39
mouadHILLALI <mauadtoto001@gmail.com>
2024-12-03 17:28:18 +0100

    PIG-5 created custom authetication provider

---
+++ src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/Impl/CustomAutheticationProvider.java
@@ -0,0 +1,35 @@
+package com.PigeonSkyRace.PigeonSkyRace.security.Impl;
+
+import com.PigeonSkyRace.PigeonSkyRace.security.CustomUserDetailsService;
+import com.PigeonSkyRace.PigeonSkyRace.security.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAutheticationProvider implements AuthenticationProvider {
+
+    private final PasswordEncoder passwordEncoder;
+    private final CustomUserDetailsService userDetailsService;
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Bad credentials");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
--- src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/Impl/CustomSecurityFilterChain.java
+++ src/main/java/com/PigeonSkyRace/PigeonSkyRace/security/Impl/CustomSecurityFilterChain.java
@@ -5,22 +5,23 @@ import com.PigeonSkyRace.PigeonSkyRace.security.PasswordEncoder;
 import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 
 @Configuration
-@RequiredArgsConstructor
+@EnableWebSecurity
 public class CustomSecurityFilterChain extends WebSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+           http.cors(customizer -> customizer.disable());
+           http.authorizeRequests(customizer -> customizer.anyRequest().authenticated());
 
-    private final PasswordEncoder passwordEncoder;
-    private final CustomUserDetailsService userDetailsService;
-//   @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//       http.authorizeRequests().requestMatchers("/api/public/**").permitAll().
-//               anyRequest().authenticated().and().
-//   }
+           return http.build();
+   }
 }
